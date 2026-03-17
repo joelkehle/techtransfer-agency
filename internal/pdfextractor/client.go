7df5fc41e9a26d34de7fd2b325f67a40dbce593b
Joel Kehle <joelkehle@users.noreply.github.com>
2026-03-17 13:29:36 -0700

    refactor: extract ucla runtime to tdg repo

--- internal/pdfextractor/client.go
+++
@@ -1,8 +0,0 @@
-package pdfextractor
-
-import "github.com/joelkehle/pinakes/pkg/busclient"
-
-type Client = busclient.Client
-type InboxEvent = busclient.InboxEvent
-
-var NewClient = busclient.NewClient
